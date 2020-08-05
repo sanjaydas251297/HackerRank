d8d20bb04fe4de153a5a009d06f8741943fedb3e
Sanjay Das <67008124+sanjaydas251297@users.noreply.github.com>
2020-08-05 23:59:34 +0530

    Add files via upload

---
+++ HackerRank_10Days_Of_Statistics/Day 2/Day 2 - More Dice.py
@@ -0,0 +1,4 @@
+#Task
+#In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+
+Solution = 1/9
