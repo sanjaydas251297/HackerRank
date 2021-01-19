305c9053f2128ec87e2d1ed71fd87ac1c14c4744
Sanjay Das <sanjaydas251297@gmail.com>
2021-01-19 23:46:06 +0530

    Changed The Complete Code. Had swapped the code with another problem.

--- HackerRank_Python/Easy/Numpy/Concatenate.py
+++ HackerRank_Python/Easy/Numpy/Concatenate.py
@@ -1,4 +1,5 @@
-import numpy
-n, m = map(int, input().split())
-array = numpy.array([input().split() for _ in range(n)], int)
-print(numpy.prod(numpy.sum(array,axis=0)))
+import numpy as np
+a, b, c = map(int,input().split())
+arrA = np.array([input().split() for _ in range(a)],int)
+arrB = np.array([input().split() for _ in range(b)],int)
+print(np.concatenate((arrA, arrB), axis = 0))
